641c98b360756a6f6bb2b052614410cdeac10f26
anatose <anatose@gmail.com>
2023-07-15 13:53:18 +0300

    A3 V2

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -4,29 +4,32 @@ import IssueModal from "../../pages/IssueModal";
 describe('Issue delete', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.url().should('eq', 'https://jira.ivorreic.com/project').then((url) => {  
-    cy.contains(issueTitle).click();
+    cy.url().should('eq', 'https://jira.ivorreic.com/project').then((url) => {
+      cy.visit(url + '/board');
+      cy.get('[data-testid="board-list:backlog"]').children().eq(0).click();
     });
+    cy.get('[placeholder="Short summary"]').invoke('text').then((title) => {
+      deletedIssue = title;
+      cy.log(`Deleting issue: "${deletedIssue}"`)
+  })
   });
 
-  const issueTitle = 'This is an issue of type: Task.';
+  let deletedIssue 
 
-  
   it('Should delete issue successfully', () => {
 
     const expectedAmountIssues = '3';
     IssueModal.clickDeleteButton();
     IssueModal.confirmDeletion();
-    IssueModal.ensureIssueIsNotVisibleOnBoard(expectedAmountIssues, issueTitle);
+    IssueModal.ensureIssueIsNotVisibleOnBoard(expectedAmountIssues, deletedIssue);
   });
 
-  
   it('Should cancel deletion process successfully', () => {
 
     const expectedAmountIssues = '4';
     IssueModal.clickDeleteButton();
     IssueModal.cancelDeletion();
     IssueModal.closeDetailModal();
-    IssueModal.ensureIssueIsVisibleOnBoard(expectedAmountIssues, issueTitle);
+    IssueModal.ensureIssueIsVisibleOnBoard(expectedAmountIssues, deletedIssue);
   });
 });
