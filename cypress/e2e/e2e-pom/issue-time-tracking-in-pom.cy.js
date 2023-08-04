5391ef9f098fde6028878988d5f87ab9361ca09f
anatose <anatose@gmail.com>
2023-08-04 21:48:17 +0300

    Sprint 2 A2

---
+++ cypress/e2e/e2e-pom/issue-time-tracking-in-pom.cy.js
@@ -0,0 +1,89 @@
+
+import IssueModal from "../../pages/IssueModal";
+
+describe('Issue delete', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', 'https://jira.ivorreic.com/project').then((url) => {
+      cy.visit(url + '/board');
+      cy.get('[data-testid="board-list:backlog"]').children().eq(0).click();
+    });
+    cy.get('[placeholder="Short summary"]').invoke('text').then((title) => {
+      firstIssue = title;
+      cy.log(`Deleting issue: "${firstIssue}"`)
+    })
+  });
+
+  let firstIssue
+
+  it('Should remove, add and change time estimation', () => {
+
+    //remove estimated and logged time from the first issue
+
+    IssueModal.clearEstimation();
+    IssueModal.closeDetailModal();
+    cy.contains(firstIssue).click();
+    IssueModal.ensureEstimationClear();
+    IssueModal.openTameTrackingModal();
+    IssueModal.ensureTimeTrackingModalIsVisible()
+    IssueModal.clearTimeLeft();
+    IssueModal.clearTimeSpent();
+    IssueModal.confirmTimeLogging();
+    IssueModal.ensureNoTimeLogged();
+
+    //add 10 hours of estimated time to the first issue
+
+    IssueModal.enterOriginalEstimateHours(10);
+    IssueModal.closeDetailModal();
+    cy.contains(firstIssue).click();
+    IssueModal.ensureEstimationIsVisible('10');
+
+    //update 10 hours to 20 in the first issue
+
+    IssueModal.clearEstimation();
+    IssueModal.enterOriginalEstimateHours(20);
+    IssueModal.closeDetailModal();
+    cy.contains(firstIssue).click();
+    IssueModal.ensureEstimationIsVisible('20');
+    IssueModal.closeDetailModal();
+
+  });
+
+
+
+  it.only('Should log time and remove logging successfully', () => {
+
+    //add spent time to the first issue
+
+    //cy.contains(firstIssue).click();
+    IssueModal.clearEstimation();
+    IssueModal.ensureEstimationClear();
+    IssueModal.enterOriginalEstimateHours(20);
+    IssueModal.openTameTrackingModal();
+    IssueModal.ensureTimeTrackingModalIsVisible()
+    IssueModal.clearTimeLeft();
+    IssueModal.clearTimeSpent();
+    IssueModal.enterTimeLeftHours(5);
+    IssueModal.enterTimeSpentHours(2);
+    IssueModal.confirmTimeLogging();
+    IssueModal.ensureNoTimeLoggedNotVisible();
+    IssueModal.ensureSpentTimeVisible('2');
+    IssueModal.ensureTimeRemainingVisible('5');
+
+
+    //remove logged time from the first issue
+
+    IssueModal.openTameTrackingModal();
+    IssueModal.ensureTimeTrackingModalIsVisible();
+    IssueModal.clearTimeLeft();
+    IssueModal.clearTimeSpent();
+    IssueModal.confirmTimeLogging();
+    //IssueModal.ensureNoTimeLogged();
+    IssueModal.closeDetailModal();
+    cy.contains(firstIssue).click();
+   // IssueModal.ensureNoTimeLogged();
+    IssueModal.ensureEstimationIsVisible('20');
+
+  });
+
+});
--- cypress/pages/IssueModal.js
+++ cypress/pages/IssueModal.js
@@ -14,6 +14,11 @@ class IssueModal {
         this.cancelDeletionButtonName = "Cancel";
         this.confirmationPopup = '[data-testid="modal:confirm"]';
         this.closeDetailModalButton = '[data-testid="icon:close"]';
+        this.timeField = 'input[placeholder="Number"]';
+        this.timeTrackingModalOpen = '[data-testid="icon:stopwatch"]';
+        this.timeTrackingModal = '[data-testid="modal:tracking"]';
+        this.doneButtonName = "Done";
+        this.estimationWindow = "Original Estimate (hours)";
     }
 
     getIssueModal() {
@@ -22,6 +27,18 @@ class IssueModal {
 
     getIssueDetailModal() {
         return cy.get(this.issueDetailModal);
+    }
+
+    openFirstIssueDetailModal() {
+        cy.get(this.backlogList).children().eq(0).click();
+    }
+
+    ensureTimeTrackingModalIsVisible() {
+        cy.get(this.timeTrackingModal).should('be.visible');
+    }
+
+    openTameTrackingModal() {
+        cy.get(this.timeTrackingModalOpen).click();
     }
 
     selectIssueType(issueType) {
@@ -39,6 +56,79 @@ class IssueModal {
     editTitle(title) {
         cy.get(this.title).debounced('type', title);
     }
+
+    clearEstimation() {
+        cy.get(this.timeField).eq(0).clear();
+        cy.wait(1000)
+    }
+
+    ensureEstimationClear() {
+        cy.contains('Original Estimate (hours)').next().within(() => {
+            cy.get('[placeholder="Number"]').should('be.visible');
+        });
+    }
+
+    enterOriginalEstimateHours(hours) {
+        cy.contains(this.estimationWindow).next().find('input').clear().debounced('type', hours);
+        cy.wait(1000)
+    }
+
+    ensureEstimationIsVisible(estimationValue) {
+        cy.contains(this.estimationWindow)
+            .next()
+            .within(() => {
+                cy.get('input').should('be.visible').and('have.value', estimationValue);
+            });
+    }
+
+    enterTimeSpentHours(hours) {
+        cy.get(this.timeField).eq(1).debounced('type', hours);
+        cy.wait(1000)
+    }
+
+    enterTimeLeftHours(hours) {
+        cy.get(this.timeField).eq(2).debounced('type', hours);
+        cy.wait(1000)
+    }
+
+    clearTimeSpent() {
+        cy.get(this.timeField).eq(1).clear();
+        cy.wait(1000)
+    }
+
+    clearTimeLeft() {
+        cy.get(this.timeField).eq(2).clear();
+        cy.wait(1000)
+    }
+
+    confirmTimeLogging() {
+        cy.get(this.timeTrackingModal).within(() => {
+            cy.contains(this.doneButtonName).click();
+            cy.wait(1000)
+        });
+        cy.get(this.timeTrackingModal).should('not.exist');
+        cy.wait(1000)
+    }
+
+    ensureNoTimeLogged() {
+        cy.contains('No time logged').should('be.visible');
+    }
+
+    ensureNoTimeLoggedNotVisible() {
+        cy.contains('No time logged').should('not.exist');
+      }
+
+      ensureTimeRemainingVisible(timeRemaining) {
+        //cy.contains(timeRemaining + "h" + " " + "estimated").next().within(() => {
+          cy.contains(timeRemaining + "h" + " " + "remaining").should('be.visible');
+        //});
+      }
+
+    ensureSpentTimeVisible(spentTime) {
+        //cy.contains(spentTime + "h" + " " + "logged").next().within(() => {
+          cy.contains(spentTime + "h" + " " + "logged").should('be.visible');
+       // });
+      }
 
     editDescription(description) {
         cy.get(this.descriptionField).type(description);
@@ -69,40 +159,39 @@ class IssueModal {
         });
     }
 
-    ensureIssueIsVisibleOnBoard(expectedAmountIssues, issueTitle){
+    ensureIssueIsVisibleOnBoard(expectedAmountIssues, issueTitle) {
         cy.get(this.issueDetailModal).should('not.exist');
         cy.reload();
         cy.contains(issueTitle).should('be.visible');
         cy.get(this.backlogList).should('be.visible').and('have.length', '1').within(() => {
             cy.get(this.issuesList)
-                .should('have.length', expectedAmountIssues)
+                .should('have.length', expectedAmountIssues);
         });
     }
 
-    ensureIssueIsNotVisibleOnBoard(expectedAmountIssues, issueTitle){
+    ensureIssueIsNotVisibleOnBoard(expectedAmountIssues, issueTitle) {
         cy.get(this.issueDetailModal).should('not.exist');
         cy.reload();
         cy.contains(issueTitle).should('not.exist');
         cy.get(this.backlogList).should('be.visible').and('have.length', '1').within(() => {
             cy.get(this.issuesList)
-                .should('have.length', expectedAmountIssues)
+                .should('have.length', expectedAmountIssues);
         });
     }
 
-    clickDeleteButton(){
+    clickDeleteButton() {
         cy.get(this.deleteButton).click();
         cy.get(this.confirmationPopup).should('be.visible');
     }
 
-    confirmDeletion(){
+    confirmDeletion() {
         cy.get(this.confirmationPopup).within(() => {
             cy.contains(this.deleteButtonName).click();
         });
         cy.get(this.confirmationPopup).should('not.exist');
-        
     }
 
-    cancelDeletion(){
+    cancelDeletion() {
         cy.get(this.confirmationPopup).within(() => {
             cy.contains(this.cancelDeletionButtonName).click();
         });
@@ -110,7 +199,7 @@ class IssueModal {
         cy.get(this.issueDetailModal).should('be.visible');
     }
 
-    closeDetailModal(){
+    closeDetailModal() {
         cy.get(this.issueDetailModal).get(this.closeDetailModalButton).first().click();
         cy.get(this.issueDetailModal).should('not.exist');
     }
